a0ced597d61298d41abc78212871df7c15f2cd80
planctus <davidepuntoferrante@gmail.com>
2020-06-22 15:38:32 +0200

    feat(storybook): an attempt - FRONT-1115

--- php/php_storybook/bootstrap.php
+++ php/php_storybook/bootstrap.php
@@ -9,10 +9,8 @@ $twig_path = __DIR__ . '/../../../node_modules/@ecl-twig';
 $twig_path_abs = Path::canonicalize($twig_path);
 $packages_folder_abs = [];
 $ec_packages_folder = Path::canonicalize(__DIR__ . '/../../src/ec/packages/');
-$eu_packages_folder = Path::canonicalize(__DIR__ . '/../../src/eu/packages/');
 
 $loader = new \Twig\Loader\FilesystemLoader($ec_packages_folder);
-$loader->addPath($eu_packages_folder, 'ecl-twig');
 $loader->addPath($ec_packages_folder, 'ecl-twig');
 $twig = new \Twig\Environment($loader);
 
