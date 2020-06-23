f1639de2cd3ade23d638e7b1f8d5d6d2f3e9cba4
planctus <davidepuntoferrante@gmail.com>
2020-06-23 10:52:45 +0200

    feat(storybook): replacing symlinks with files in EU - FRONT-1115

--- src/eu/storybook/.storybook/environment.js
+++ src/eu/storybook/.storybook/environment.js
@@ -1,1 +1,14 @@
-../../../ec/storybook/.storybook/environment.js
+const path = require('path');
+const { TwingEnvironment, TwingLoaderFilesystem } = require('twing');
+
+const ecAbsPath = path.resolve('src/ec/packages');
+const loader = new TwingLoaderFilesystem(ecAbsPath);
+
+// In storybook we get this returned as an instance of
+// TWigLoaderNull, we need to avoid processing this.
+if (typeof loader.addPath === 'function') {
+  // Add namespace ecl-twig.
+  loader.addPath(ecAbsPath, 'ecl-twig');
+}
+
+module.exports = new TwingEnvironment(loader);
