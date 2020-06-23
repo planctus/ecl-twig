d145b096b33f01ec57a86570d5e34f3ad8ba8bc8
planctus <davidepuntoferrante@gmail.com>
2020-06-23 11:23:44 +0200

    feat(storybook): progress plugin for php storybook, deploying EC only - FRONT-1115

--- php/ec/.storybook/webpack.config.js
+++ php/ec/.storybook/webpack.config.js
@@ -1,10 +1,10 @@
 module.exports = ({ config: defaultConfig, mode }) => {
   // Make it less verbose
   if (mode === 'PRODUCTION') {
-    // Remove ProgressPlugin (4th plugin)
-    const plugin = defaultConfig.plugins.splice(3, 1);
+    // Remove ProgressPlugin (5th plugin)
+    const plugin = defaultConfig.plugins.splice(4, 1);
     if (plugin[0].constructor.name !== 'ProgressPlugin') {
-      defaultConfig.plugins.push(plugin[0]);
+      defaultConfig.plugins.splice(4, 0, plugin);
     }
   }
 
--- src/eu/storybook/.storybook/webpack.config.js
+++ src/eu/storybook/.storybook/webpack.config.js
@@ -15,7 +15,7 @@ module.exports = ({ config: defaultConfig, mode }) => {
     // Remove ProgressPlugin (5th plugin)
     const plugin = defaultConfig.plugins.splice(4, 1);
     if (plugin[0].constructor.name !== 'ProgressPlugin') {
-      defaultConfig.plugins.push(plugin[0]);
+      defaultConfig.plugins.splice(4, 0, plugin);
     }
   }
 
